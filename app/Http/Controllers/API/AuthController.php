ed19771c4c65d90e4062170038b4ec4bad469129
khalifa <mednaceur199530@gmail.com>
2022-10-05 16:10:40 +0100

    auth controller

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends BaseController
+{
+    public function Login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!$this->guard()->attempt($credentials)) {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised'], 422);
+        }
+        $this->guard()->attempt($credentials);
+        $token = $this->guard()->user()->createToken('auth-token')->plainTextToken;
+        return $this->sendResponse([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 'user logged in  Succesfuly');
+    }
+
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->tokens()->delete();
+        $this->guard()->logout();
+        return $this->sendResponse(['status_code' => '200'], 'logged out successfully');
+    }
+
+    public function guard($guard = 'web')
+    {
+        return Auth::guard($guard);
+    }
+}
