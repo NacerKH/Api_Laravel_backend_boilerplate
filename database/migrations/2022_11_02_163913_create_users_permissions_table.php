ff4002da5452b8dd04509a4bc137fd7605aaef8d
khalifa <mednaceur199530@gmail.com>
2022-11-02 17:40:47 +0100

    make migration permissions

---
+++ database/migrations/2022_11_02_163913_create_users_permissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_permissions', function (Blueprint $table) {
+            $table->foreignId('user_id')->onDelete('cascade');
+        $table->foreignId('permission_id')->onDelete('cascade');
+
+      
+
+        //PRIMARY KEYS
+        $table->primary(['user_id','permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_permissions');
+    }
+};
