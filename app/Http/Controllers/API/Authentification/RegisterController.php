d1db6658bccbcd8cd66815e1cab4eecfe939b0dd
khalifa <mednaceur199530@gmail.com>
2022-11-03 17:31:40 +0100

    user attach with role_user

--- app/Http/Controllers/API/Authentification/RegisterController.php
+++ app/Http/Controllers/API/Authentification/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\Authentification;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
@@ -32,6 +33,8 @@ class RegisterController extends BaseController
         $user = User::create($data);
         if ($user) {
             event(new Registered($user));
+            $user_role = Role::where('slug','user')->first();
+            $user->roles()->attach($user_role);
             $success['token'] =  $user->createToken('auth-token')->plainTextToken;
             $success['name'] =  $user->name;
 
