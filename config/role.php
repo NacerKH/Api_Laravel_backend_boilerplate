2353110f4c6e8a250664d91b2585c7c7530e4c2d
khalifa <mednaceur199530@gmail.com>
2022-11-03 17:21:22 +0100

    make config role

---
+++ config/role.php
@@ -0,0 +1,18 @@
+<?php
+
+   /*
+    |--------------------------------------------------------------------------
+    | Role
+    |--------------------------------------------------------------------------
+    |
+    | This file is for enable or disable working with separate
+    | role table and permission or using field in table user
+    |
+    |
+    |
+    */
+return [
+    'role' => [
+        'enable' => env('ENBLE_ROLE_TABLE',TRUE)
+    ],
+];
