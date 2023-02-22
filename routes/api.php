5268e1fcbaae36e290dedb0079dcb878e349b326
NaserKh <naserK@360medlink.com>
2023-02-22 17:59:05 +0100

    make routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\API\Authentification\ForgotPasswordController;
 use App\Http\Controllers\API\Authentification\RegisterController;
 use App\Http\Controllers\API\Authentification\Verification\VerificationController;
 use App\Http\Controllers\API\User\SocialController;
+use App\Http\Controllers\API\User\TwoFactoryAuthenticableController;
 use App\Http\Controllers\API\User\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,22 +22,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/verify-email/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify');;
+    Route::get('/verify-email/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify');;
     Route::post('/verify-resend', [VerificationController::class, 'resend']);
 
     ## Update user informations Profil & Password
-    Route::post('/updateUserInformationProfil',[UserController::class, 'update'])->name('user.updateProfil');
-    Route::post('/updateUserPassword',[UserController::class, 'updatePassword'])->name('user.update.password');
+    Route::post('/updateUserInformationProfil', [UserController::class, 'update'])->name('user.updateProfil');
+    Route::post('/updateUserPassword', [UserController::class, 'updatePassword'])->name('user.update.password');
+    Route::post('/enable-mfa', [TwoFactoryAuthenticableController::class, 'storeTwoFactoryAuthenticable'])->name('user.enable.mfa');
+    Route::post('/confirm-mfa-secretkey/{code}', [TwoFactoryAuthenticableController::class, 'confirmationTwoFactoryAuthenticable'])->name('user.confirm.mfa.secret');
+    Route::get('/recovery-codes', [TwoFactoryAuthenticableController::class, 'getRecoveryCodes'])->name('user.recovery.codes');
+    Route::get('/disable-mfa', [TwoFactoryAuthenticableController::class, 'disableMfa'])->name('user.disable.mfa');
+    Route::post('/confirm-mfa-recovery-code/{code}', [TwoFactoryAuthenticableController::class, 'confirmMfaWithRecoveryCodes'])->name('user.confirm.mfa.recovery.code');
+    Route::post('/regenerate-recovery-codes', [TwoFactoryAuthenticableController::class, 'regenerateNewRecoveryCodes'])->name('user.regnerate.mfa.recovery.codes');
 });
 
 Route::middleware('guest')->group(function () {
     # guest auth
     Route::post('/login', [AuthController::class, 'login'])->name('user.login');
-    Route::post('/register', [RegisterController::class,'register'])->name('user.register');
+    Route::post('/register', [RegisterController::class, 'register'])->name('user.register');
     # guest reset password (temporary link)
-    Route::post('/PasswordResetLink',[ForgotPasswordController::class, 'PasswordResetLink'])->name('PasswordResetLink');
+    Route::post('/PasswordResetLink', [ForgotPasswordController::class, 'PasswordResetLink'])->name('PasswordResetLink');
 
-    Route::post('/ForgotPassword',[ForgotPasswordController::class, 'reset'])->name('password.reset');
+    Route::post('/ForgotPassword', [ForgotPasswordController::class, 'reset'])->name('password.reset');
     Route::get('/redirect/{service}', [SocialController::class, 'redirect'])->name('redirect');
 
     Route::get('/callback/{service}', [SocialController::class, 'callback'])->name('callback');
