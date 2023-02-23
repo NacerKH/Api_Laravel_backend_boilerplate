bed5c02ff58001a277c1197d9116a569c2eaea32
NaserKh <naserK@360medlink.com>
2023-02-23 10:39:59 +0100

    register Mfa in Api group middlaware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\IsAdminMiddleware;
+use App\Http\Middleware\MfaMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -49,6 +50,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            MfaMiddleware::class,
         ],
     ];
 
