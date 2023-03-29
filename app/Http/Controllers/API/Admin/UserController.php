44889c2b7c2e673a214d97e779ec7f4502fc3ba9
NaserKh <naserK@360medlink.com>
2023-03-29 18:20:31 +0100

    fix directory and add cors Middleware

---
+++ app/Http/Controllers/API/Admin/UserController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin;
+
+use App\Http\Controllers\API\BaseController;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UsersRequest;
+use App\Http\Resources\UserResource;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserController extends  BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users=User::with(['roles','permissions'])->get();
+
+        return $this->sendResponse(
+            UserResource::collection(User::with(['roles','permissions'])->paginate(20))->response()->getData(true)
+            ,"Getting  Your Users Successfully  !");
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UsersRequest $request)
+    {
+        $validatedUser = $request->validated();
+        $validatedUser['password'] = bcrypt($validatedUser['password']);
+        $user_role = Role::where('slug',$validatedUser['role'])->first();
+        unset($validatedUser['role']) ;
+        $user = User::create($validatedUser);
+        $user->roles()->attach($user_role);
+
+
+        return $this->sendResponse(UserResource::make($user) ,'User Added successfully !');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user=User::with(['roles'])->find($id);
+        return $this->sendResponse(UserResource::make($user) ,"user retrieved successfully");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UsersRequest $request, $id)
+    {
+
+     abort_unless(auth()->user->hasRole('admin'),Response::HTTP_UNAUTHORIZED,'You are Not Allowed to destroy this User');
+        $validatedUser = $request->validated();
+        $user=User::findOrfail($id);
+        $user->update($validatedUser);
+        return $this->sendResponse(UserResource::make($user) ,'User showed successfully !');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        abort_unless(auth()->user->hasRole('admin'),Response::HTTP_UNAUTHORIZED,'You are Not Allowed to destroy this User');
+        $user=User::findOrfail($id);
+        $user->deleteProfilePhoto();
+        $user->delete();
+        return response()->noContent();
+    }
+
+
+  
+    public function getInfo()
+    {  
+        abort_unless(auth()->user()->hasRole('admin'), Response::HTTP_UNAUTHORIZED, 'You are Not Allowed to destroy this User');
+        $user = request()->user()->load(['roles']);
+        return $this->sendResponse(UserResource::make($user), "user retrieved successfully");
+    }
+}
---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }
+}
